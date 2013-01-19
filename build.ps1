9c33146f6d0749650c96e00aa48abc2bc0ab4e04
Andrew Smith <devnull@localhost>
2013-01-19 00:41:43 +0000

    bump version after bug fix

--- build.ps1
+++ build.ps1
@@ -4,7 +4,7 @@ Properties {
 	$buildArtifactsDir = "$buildDir\_build"
 	$projectOutputDir = "$buildArtifactsDir\bin\"
 	$solutionDir = "$buildDir"
-	$version = "0.2.0"
+	$version = "0.2.1"
 }
 
 Task default -Depends Compile, RunTests, CreatePackage
