c21ca1a12a3e616a22ad65768956dea8d1b6b250
Andrew Smith <devnull@localhost>
2013-12-01 20:23:03 +0000

    bump version to 0.2.6

--- build.ps1
+++ build.ps1
@@ -4,7 +4,7 @@ Properties {
 	$buildArtifactsDir = "$buildDir\_build"
 	$projectOutputDir = "$buildArtifactsDir\bin\"
 	$solutionDir = "$buildDir"
-	$version = "0.2.5"
+	$version = "0.2.6"
 }
 
 Task default -Depends Compile, RunTests, CreatePackage
